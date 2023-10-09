195e1f324e12152f42423ae05b5c1820962fd2a5
Maxime Roucher <maximero@theodo.fr>
2023-10-09 20:20:51 +0200

    feat: adding Day

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:formationflutter/day.dart';
 
 class Calendar extends StatelessWidget {
   const Calendar({super.key});
@@ -22,6 +23,8 @@ class Calendar extends StatelessWidget {
             color: const Color.fromARGB(255, 33, 59, 209),
             borderRadius: BorderRadius.circular(45)),
         height: 130,
+        child: Row(
+            children: daysAndNumber.map((e) => Day(dayAndNumber: e)).toList()),
       ),
     );
   }
---
+++ lib/day.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Day extends StatelessWidget {
+  final Map<String, int> dayAndNumber;
+  const Day({super.key, required this.dayAndNumber});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
