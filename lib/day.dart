0140665042a2be2a6fdc543acd2b5f0c96470a7b
Maxime Roucher <maximero@theodo.fr>
2023-10-11 12:30:17 +0200

    feat: adding day style

--- lib/day.dart
+++ lib/day.dart
@@ -6,6 +6,25 @@ class Day extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      width: 40,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            dayAndNumber.keys.first,
+            style: const TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Text(
+            dayAndNumber.values.first.toString(),
+            style: const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
+          )
+        ],
+      ),
+    );
   }
 }
