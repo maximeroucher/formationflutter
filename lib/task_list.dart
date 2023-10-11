e30c20d9fb02da4b2c1b236dff64c88546809e4a
Maxime Roucher <maximero@theodo.fr>
2023-10-11 20:59:42 +0200

    feat: adding stack

--- lib/task_list.dart
+++ lib/task_list.dart
@@ -16,13 +16,39 @@ class TaskList extends StatelessWidget {
     ];
 
     return Expanded(
-      child: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Column(
-          children: tasks
-              .map((e) => TaskUi(task: e, selected: tasks.indexOf(e) == 0))
-              .toList(),
-        ),
+      child: Stack(
+        children: [
+          SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              children: tasks
+                  .map((e) => TaskUi(task: e, selected: tasks.indexOf(e) == 0))
+                  .toList(),
+            ),
+          ),
+          Positioned(
+            bottom: 30,
+            left: MediaQuery.of(context).size.width / 2 - 40,
+            child: Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(40),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color.fromARGB(255, 60, 76, 200).withOpacity(0.2),
+                    spreadRadius: 5,
+                    blurRadius: 15,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: const Icon(Icons.add,
+                  color: Color.fromARGB(255, 60, 76, 200), size: 40),
+            ),
+          )
+        ],
       ),
     );
   }
